afb3d147ef32da14f9916ad56941b4c4573d5cf2
Guillermo Moreno <gmq@guillermomoreno.cl>
2020-03-30 14:54:17 -0300

    fix(): fix google fonts stylesheet

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,7 @@ const sidebarGenerator = require('./config/sidebar-generator');
 module.exports = {
   base: '/component-library/',
   head: [
-    ['link', 'https://fonts.googleapis.com/css?family=Rubik:300,400,500,700&display=swap'],
+    ['link', { href: 'https://fonts.googleapis.com/css?family=Rubik:300,400,500,700&display=swap', rel: 'stylesheet' }],
   ],
   themeConfig: {
     sidebar: [
